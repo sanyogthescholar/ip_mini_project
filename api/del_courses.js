342a91fd69a37c47930e2e951e15db0ebe861c4e
Sanyog Jain <sanyogjain5678@gmail.com>
2022-10-12 22:52:31 +0530

    Add function to delete courses

---
+++ api/del_courses.js
@@ -0,0 +1,40 @@
+import { parse } from "url";
+import { MongoClient } from "mongodb";
+
+let cachedDb = null;
+const uri = process.env.VISITORSDB;
+
+async function connectToDatabase()
+{
+    if(cachedDb){
+        return cachedDb;
+    }
+
+    const client = await MongoClient.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const db = await client.db(parse(uri).pathname.substr(1));
+
+    cachedDb = db;
+    console.log("Connected to db")
+    return db;
+}
+
+export default async (req, res) => {
+    try {
+        console.log("Inside default log function");
+
+        const db = await connectToDatabase();
+        const collection = await db.collection("courses");
+        let apos = await collection.drop();
+        //console.log(apos)
+        //console.log("Fetched data")
+        }
+        catch(error)
+        {
+            console.log(error);
+            res.status(500).send();
+        }
+};
